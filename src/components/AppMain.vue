e37e2389ce8e488d7f72c36c00c053ae1e1d449e
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-03-28 15:06:48 +0200

    Crezione componenete main con le card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,20 +1,101 @@
 <script>
+import Card from './Card.vue';
 export default {
     name: "AppMain",
+    components:{
+        Card
+    },
     data() {
         return {
+            cards:[
+                        {
+                            "thumb": "https://static.dc.com/2023-03/LEGIONOB_Cv1_00111_DIGITAL.jpg?w=640",
+                            "price": "$19.99",
+                            "series": "Action Comics",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/BA_Cv9_00911_DIGITAL.jpg?w=640",
+                            "price": "$3.99",
+                            "series": "American Vampire 1976",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/ST_GH_Cv3_00311_DIGITAL.jpg?w=640",
+                            "price": "$16.99",
+                            "series": "Aquaman",
+                            "type": "graphic novel"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/MS30A_Cv1_00111_DIGITAL.jpg?w=640",
+                            "price": "$2.99",
+                            "series": "Batgirl",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/MS30A_Cv1_00111_DIGITAL.jpg?w=640", 
+                            "price": "$3.99",
+                            "series": "Batman",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/BMSMWF_Cv13_01311_DIGITAL.jpg?w=640",
+                            "price": "$2.99",
+                            "series": "Batman Beyond",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/NTW_Cv102_10211_DIGITAL-1.jpg?w=640",
+                            "price": "$3.99",
+                            "series": "Batman/Superman",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/FLS_Cv795_79511_DIGITAL.jpg?w=640",
+                            "price": "$4.99",
+                            "series": "Batman/Superman Annual",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/DCConnect_35-CVR.jpg?w=640",
+                            "price": "$5.99",
+                            "series": "Batman: The Joker War Zone",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/ROBIN_v3%20%28Cover%29.jpg?w=640",
+                            "price": "$6.99",
+                            "series": "Batman: Three Jokers",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/LP-ROTG_Cv1_00111_DIGITAL.jpg?w=640",
+                            "price": "$4.99",
+                            "series": "Batman: White Knight Presents: Harley Quinn",
+                            "type": "comic book"
+                        },
+                        {
+                            "thumb": "https://static.dc.com/2023-03/SM_LOST_Cv1_00111_DIGITAL.jpg?w=640",
+                            "price": "$16.99",
+                            "series": "Catwoman",
+                            "type": "graphic novel"
+                        }
+                        ]
         }
     }
 }
 </script>
 
 <template>
-
-<main>
+    
+    <main>
         <div class="container">
-                 Content goes here
-        </div>
 
+            <div class="cards">
+                <Card v-for="card in cards" :thumb="card" />
+            </div>
+
+        </div>
     </main>
 
 </template>
@@ -26,7 +107,12 @@ main {
     background-color: #1c1c1c;
     .container {
         color: $text-white;
-        padding: 40px 0;
+        .cards {
+            display: flex;
+            flex-wrap: wrap;
+            margin-inline: -10px;
+            padding: 50px 0;
+        }
     }
 }
 
---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "Card",
+    props: {
+        thumb: Object
+    }
+}
+</script>
+
+<template>
+
+    <div class="card">
+        <div>
+            <img :src="thumb.thumb" :alt="thumb.series">
+        </div>
+        <h6>{{ thumb.series.toUpperCase() }}</h6>
+    </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+.card {
+    width: calc(100% / 6 - 30px);
+    margin: 20px 15px;
+    h6{
+        margin-top: 10px;
+    }
+}
+</style>
